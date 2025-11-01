11b52d47a99d996afdfa4661a409b3b09dd88a3c
RAVADA HEMANTH <2300032944@kluniversity.in>
2025-11-01 15:20:28 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,33 +3,24 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import axios from 'axios';
-
-// Pages
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Home from './pages/Home';
 import History from './pages/History';
 import Profile from './pages/Profile';
 import OAuthCallback from './pages/OAuthCallback';
-
-// Components
 import Navbar from './components/Navbar';
 import Welcome from './components/Welcome';
-
-// Context
 import { AuthProvider } from './context/AuthContext';
 
-// CSS
 import './App.css';
 
-// Set default axios baseURL
 axios.defaults.baseURL = 'http://localhost:5000';
 axios.defaults.withCredentials = true;
 
 function App() {
   const [theme, setTheme] = useState('light');
 
-  // Toggle theme
   const toggleTheme = () => {
     const newTheme = theme === 'light' ? 'dark' : 'light';
     setTheme(newTheme);
@@ -37,7 +28,6 @@ function App() {
     localStorage.setItem('theme', newTheme);
   };
 
-  // Load theme from localStorage on initial render
   useEffect(() => {
     const savedTheme = localStorage.getItem('theme') || 'light';
     setTheme(savedTheme);
